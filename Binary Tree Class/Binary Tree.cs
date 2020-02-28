d11356a3b00ce7f45b84eef99b6ae12140779733
Jonathan Friedl <jfreetz2@gmail.com>
2020-02-28 16:37:39 -0600

    got basic stuff in but still researching trying to understand what to do

--- Binary Tree Class/Binary Tree.cs
+++ Binary Tree Class/Binary Tree.cs
@@ -8,5 +8,37 @@ namespace Binary_Tree_Class
 {
     class Binary_Tree
     {
+        Node start;
+        Node tail;
+
+        public bool Search(Node data)
+        {
+            if (//data value< this node go left)
+            {
+
+            }
+            if (//data value >= this node go right)
+            {
+
+            }
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node node = new Node(data); //10
+            if (start == null)
+            {
+                start = node;
+                return;
+            }
+            node.link = tail;
+
+            //Node temp = start; //7
+            //while (temp.link != null)
+            //{
+            //    temp = temp.link;
+            //}
+            //temp.link = node;
+        }
     }
 }
--- Binary Tree Class/Node.cs
+++ Binary Tree Class/Node.cs
@@ -8,5 +8,14 @@ namespace Binary_Tree_Class
 {
     class Node
     {
+        public Node left;
+        public Node right;
+        int data;
+        public Node link;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
     }
 }
